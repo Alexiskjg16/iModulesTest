a5a0e2cd70c90d809a42473246472fe5ebe52209
Alexis K Grisham <alexisgrisham@Leenie.localdomain>
2018-11-19 13:56:47 -0500

    added second level

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
+import headerbackground from './Images/headerbackground.png';
+import image1 from './Images/image1.png';
+import image2 from './Images/image2.png';
+import image3 from './Images/image3.png';
+import imoduleslogo from './Images/imodules-logo.png';
 
 class App extends Component {
   render() {
@@ -8,32 +13,30 @@ class App extends Component {
       <Router>
         <div className="App">
         <nav className="App-header">
-          <header> imodules </header>
-          <ul>
-            <li><Link to="/">About</Link></li>
-            <li><Link to="/">Solutions</Link></li>
-            <li><Link to="/">News</Link></li>
-            <li><Link to="/">Events</Link></li>
-            <li><Link to="/">Contact</Link></li>
-          </ul>
+          <img src={imoduleslogo} alt="iModules Logo" className="logoTag"/>
+            <Link to="/">About</Link>
+            <Link to="/">Solutions</Link>
+            <Link to="/">News</Link>
+            <Link to="/">Events</Link>
+            <Link to="/">Contact</Link>
         </nav>
-        <img alt="headerimg"></img>
-        <section>
-          <menu>
-            <Link to="/">Our Technology</Link>
-            <Link to="/">The Client Experience</Link>
-            <Link to="/">Leadership</Link>
-            <Link to="/">Partners</Link>
-            <Link to="/">Careers</Link>
-            <Link to="/">Contact Us</Link>
+        <img src={headerbackground} alt="headerimg" className="headerImg"/>
+        <section className="bodyContent">
+          <menu className="sideList">
+            <Link to="/" className="sideLinks">Our Technology</Link>
+            <Link to="/" className="sideLinks">The Client Experience</Link>
+            <Link to="/" className="sideLinks">Leadership</Link>
+            <Link to="/" className="sideLinks">Partners</Link>
+            <Link to="/" className="sideLinks">Careers</Link>
+            <Link to="/" className="sideLinks">Contact Us</Link>
           </menu>
-        <section>
+        <section className="paragraphsBody">
           <h1>Main Header Sits Here</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
             magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <h2>Secondary Content Header</h2>
+          <h2>SECONDARY CONTENT HEADER</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
             magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
@@ -41,20 +44,20 @@ class App extends Component {
         </section>
         </section>
         <section>
-          <img alt="support student success"></img>
-          <img alt="upcoming campus events"></img>
-          <img alt="read alumni stories"></img>
+          <img src={image1} alt="support student success" />
+          <img src={image2} alt="upcoming campus events" />
+          <img src={image3} alt="read alumni stories" />
         </section>
-       <footer>
-         <section>
-          <h3>iModules</h3>
+       <footer className="wholeFooter">
+         <section className="mainPartFooter">
+         <img src={imoduleslogo} alt="iModules Logo"  className="logoTag" />
           <section>
            <h4>5101 College Blvd, Leawood, KS 66211</h4>
            <h4>913.888.0772 - info@imodules.com</h4>
           </section>
-          <button>Donate today</button>
+          <button className="donateButton">Donate today</button>
           </section>
-          <section>
+          <section className="baseOfTheFooter">
             <h1>Thanks for completing this test build!</h1>
           </section>
        </footer>
