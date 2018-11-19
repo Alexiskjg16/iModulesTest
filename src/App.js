ef0199ff7c01d42cdb8cf6a9f7930555f828e99b
Alexis K Grisham <alexisgrisham@Leenie.localdomain>
2018-11-19 16:57:29 -0500

    fixed opacity issue

--- src/App.js
+++ src/App.js
@@ -35,7 +35,8 @@ class App extends Component {
           <p className="LoremPart">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
             magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <Link to="/">Link Content Example</Link></p>
           <p id="secondaryHeader">SECONDARY CONTENT HEADER</p>
           <p className="LoremPart">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
             magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
