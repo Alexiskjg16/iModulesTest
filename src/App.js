ef8d19da295b2680ab5e5045b3d3e8ab28847672
Alexis K Grisham <alexisgrisham@Leenie.localdomain>
2018-11-19 16:31:31 -0500

    added many classes

--- src/App.js
+++ src/App.js
@@ -14,11 +14,11 @@ class App extends Component {
         <div className="App">
         <nav className="App-header">
           <img src={imoduleslogo} alt="iModules Logo" className="logoTag"/>
-            <Link to="/">About</Link>
-            <Link to="/">Solutions</Link>
-            <Link to="/">News</Link>
-            <Link to="/">Events</Link>
-            <Link to="/">Contact</Link>
+            <Link to="/" className="specificNav">About</Link>
+            <Link to="/" className="specificNav">Solutions</Link>
+            <Link to="/" className="specificNav">News</Link>
+            <Link to="/" className="specificNav">Events</Link>
+            <Link to="/" className="specificNav">Contact</Link>
         </nav>
         <img src={headerbackground} alt="headerimg" className="headerImg"/>
         <section className="bodyContent">
@@ -31,22 +31,31 @@ class App extends Component {
             <Link to="/" className="sideLinks">Contact Us</Link>
           </menu>
         <section className="paragraphsBody">
-          <h1>Main Header Sits Here</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
+          <h1 id="mainHeader">Main Header Sits Here</h1>
+          <p className="LoremPart">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
             magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <h2>SECONDARY CONTENT HEADER</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
+          <p id="secondaryHeader">SECONDARY CONTENT HEADER</p>
+          <p className="LoremPart">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
             magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </section>
         </section>
-        <section>
-          <img src={image1} alt="support student success" />
+        <section className="threeImages">
+          <section className="fullPicture">
+          <img src={image1} alt="support student success"/>
+          <h3 id="SupportSuccess">Support Student Success</h3>
+          </section>
+          <section className="fullPicture">
           <img src={image2} alt="upcoming campus events" />
+          <h3 id="UpcomingEvents">Upcoming Campus Events</h3>
+          </section>
+          <section className="fullPicture">
           <img src={image3} alt="read alumni stories" />
+          <h3 id="alumni">Read Alumni Stories</h3>
+          </section>
         </section>
        <footer className="wholeFooter">
          <section className="mainPartFooter">
